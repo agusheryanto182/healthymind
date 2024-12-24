b58ce1ecca131c5fdd5288ef13bc1c0833be05ad
Agus Heryanto <agusheryantodev@gmail.com>
2024-12-24 15:17:37 +0700

    feat: update consult button components

--- components/consult-button.tsx
+++ components/consult-button.tsx
@@ -28,31 +28,33 @@ export default function ConsultButton({ data }: any) {
 
       {/* Modal */}
       {isModalOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="w-3/4 max-w-lg rounded-lg bg-white p-6 shadow-lg">
-            <h2 className="mb-4 text-lg font-bold text-gray-800">
-              Pilih Kontak
-            </h2>
-            <ul>
-              {contacts.map((contact, index) => (
-                <li key={index} className="mb-2">
-                  <a
-                    href={contact.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="block w-full rounded-lg border border-cyan-500 bg-cyan-100 p-3 text-cyan-700 hover:bg-cyan-200"
-                  >
-                    {contact.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-            <button
-              onClick={() => setIsModalOpen(false)}
-              className="mt-4 rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-600"
-            >
-              Tutup
-            </button>
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 px-4">
+          <div className="w-full max-w-lg rounded-lg bg-white p-6 shadow-lg">
+            <div className="flex flex-col items-center justify-center ">
+              <h2 className="mb-4 text-center text-lg font-bold text-gray-800">
+                Pilih Kontak
+              </h2>
+              <ul>
+                {contacts.map((contact, index) => (
+                  <li key={index} className="mb-2">
+                    <a
+                      href={contact.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="block w-full rounded-lg border border-cyan-500 bg-cyan-100 p-3 text-cyan-700 hover:bg-cyan-200"
+                    >
+                      {contact.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <button
+                onClick={() => setIsModalOpen(false)}
+                className="mt-4 rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-600"
+              >
+                Tutup
+              </button>
+            </div>
           </div>
         </div>
       )}
