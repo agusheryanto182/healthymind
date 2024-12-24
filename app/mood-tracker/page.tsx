c5f13407554a851b7a030cd9c1d7b0f0db7f8ae9
Agus Heryanto <agusheryantodev@gmail.com>
2024-12-24 15:43:35 +0700

    feat: update mood tracker pages

--- app/mood-tracker/page.tsx
+++ app/mood-tracker/page.tsx
@@ -3,9 +3,11 @@ import MoodTrackerComponent from "@/components/mood-tracker";
 
 export default function Page() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+    <div className="flex flex-1 flex-col gap-4  pt-0">
       <BreadcrumbComponent title="Mood Tracker" />
-      <MoodTrackerComponent />
+      <div className="p-4">
+        <MoodTrackerComponent />
+      </div>
     </div>
   );
 }
